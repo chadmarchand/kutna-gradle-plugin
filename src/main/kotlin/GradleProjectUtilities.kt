67d3b3d510187b44b2a3eaf2afe2480f4b234f19
chadmarchand <37517821+chadmarchand@users.noreply.github.com>
2020-10-20 21:06:39 -0500

    Add additional common dependencies to plugin (#2)

---
+++ src/main/kotlin/GradleProjectUtilities.kt
@@ -0,0 +1,71 @@
+package com.chadmarchand.kutna.gradle
+
+import org.gradle.api.Project
+
+internal fun Project.addTestImplementationDependency(
+    group: String,
+    name: String,
+    version: String?
+) {
+    this.addDependency(
+        "testImplementation",
+        group,
+        name,
+        version
+    )
+}
+
+internal fun Project.addTestRuntimeDependency(
+    group: String,
+    name: String,
+    version: String?
+) {
+    this.addDependency(
+        "testRuntimeOnly",
+        group,
+        name,
+        version
+    )
+}
+
+internal fun Project.addImplementationDependency(
+    group: String,
+    name: String,
+    version: String?
+) {
+    this.addDependency(
+        "implementation",
+        group,
+        name,
+        version
+    )
+}
+
+internal fun Project.addDependency(
+    dependencyType: String,
+    group: String,
+    name: String,
+    version: String?
+) {
+    this.dependencies.add(
+        dependencyType,
+        createDependencyModel(group, name, version)
+    )
+}
+
+private fun createDependencyModel(
+    group: String,
+    name: String,
+    version: String?
+): Map<String, String> {
+    val sharedModel = hashMapOf(
+        Pair("group", group),
+        Pair("name", name)
+    )
+
+    if (version != null) {
+        sharedModel["version"] = version
+    }
+
+    return sharedModel
+}
--- src/main/kotlin/KutnaGradlePlugin.kt
+++ src/main/kotlin/KutnaGradlePlugin.kt
@@ -3,46 +3,33 @@ package com.chadmarchand.kutna.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-private fun createSharedModel(group: String, name: String, version: String?): Map<String, String> {
-    val sharedModel = hashMapOf(
-        Pair("group", group),
-        Pair("name", name)
-    )
-
-    if (version != null) {
-        sharedModel["version"] = version
-    }
-
-    return sharedModel
-}
-
-private fun addTestImplementationDependency(
-    project: Project,
-    group: String, name:
-    String,
-    version: String
-) {
-    val sharedModel = createSharedModel(group, name, version)
-    project.dependencies.add("testImplementation", sharedModel)
-}
-
-private fun addTestRuntimeDependency(
-    project: Project,
-    group: String, name:
-    String,
-    version: String
-) {
-    val sharedModel = createSharedModel(group, name, version)
-    project.dependencies.add("testRuntimeOnly", sharedModel)
-}
-
-private fun addTestDependencies(project: Project) {
-    addTestImplementationDependency(project, "org.junit.jupiter", "junit-jupiter-api", "5.4.2")
-    addTestRuntimeDependency(project, "org.junit.jupiter", "junit-jupiter-engine", "5.4.2")
-}
-
 class KutnaGradlePlugin : Plugin<Project>{
     override fun apply(project: Project) {
-        addTestDependencies(project)
+        addKotlinDependencies(project)
+        addJUnitDependencies(project)
+        addKoinDependencies(project)
+    }
+
+    private fun addKotlinDependencies(project: Project) {
+        project.addImplementationDependency("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", null)
+    }
+
+    private fun addKoinDependencies(project: Project) {
+        val koinGroup = "org.koin"
+        val koinVersion = "2.1.6"
+        val koinJavaVersion = "2.1.0-alpha-2"
+
+        project.addImplementationDependency(koinGroup, "koin-core", koinVersion)
+        project.addImplementationDependency(koinGroup, "koin-core-ext", koinVersion)
+        project.addImplementationDependency(koinGroup, "koin-java", koinJavaVersion)
+        project.addTestImplementationDependency(koinGroup, "koin-test", koinVersion)
+    }
+
+    private fun addJUnitDependencies(project: Project) {
+        val junitGroup = "org.junit.jupiter"
+        val junitVersion = "5.4.2"
+
+        project.addTestImplementationDependency(junitGroup, "junit-jupiter-api", junitVersion)
+        project.addTestRuntimeDependency(junitGroup, "junit-jupiter-engine", junitVersion)
     }
 }
