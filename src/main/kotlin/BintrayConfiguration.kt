3b7905c7e7df209f43e701e63beb025aeef7b855
chadmarchand <37517821+chadmarchand@users.noreply.github.com>
2020-12-12 10:05:18 -0600

    Throw error if bintray credentials not defined (#16)

--- src/main/kotlin/BintrayConfiguration.kt
+++ src/main/kotlin/BintrayConfiguration.kt
@@ -8,6 +8,12 @@ private const val BINTRAY_USER_ENV_VARIABLE = "BINTRAY_USER"
 private const val BINTRAY_KEY_ENV_VARIABLE = "BINTRAY_KEY"
 private const val MIT_LICENSE = "MIT"
 
+private fun getRequiredEnvironmentVariable(name: String): String {
+    return System.getenv(name) ?: throw SystemVariableAccessException(
+        "Environment variable ${name} is not defined"
+    )
+}
+
 internal fun Project.configureBintrayExtension(
     repo: String,
     projectName: String,
@@ -18,8 +24,8 @@ internal fun Project.configureBintrayExtension(
         this.repoName = repo
         this.packageName = projectName
         this.versionName = version
-        this.user = System.getenv(BINTRAY_USER_ENV_VARIABLE)
-        this.apiKey = System.getenv(BINTRAY_KEY_ENV_VARIABLE)
+        this.user = getRequiredEnvironmentVariable(BINTRAY_USER_ENV_VARIABLE)
+        this.apiKey = getRequiredEnvironmentVariable(BINTRAY_KEY_ENV_VARIABLE)
         this.setPublications(MAVEN_PUBLICATION_NAME)
         this.publish = true
         this.versionReleased = Date.from(Instant.now()).toString()
